816ba04cc8c4c9436cfc430c3ed6d5d5aa31d99c
George DS Brooks <george.ds.brooks@gmail.com>
2022-03-27 10:34:16 +0100

    working mvp

--- src/App.js
+++ src/App.js
@@ -1,66 +1,82 @@
 import './App.css';
-import { Avatar, Container, Stack, Typography, Box, ButtonGroup, Button, Badge, Paper, IconButton, List } from '@mui/material';
+import { Avatar, Container, Stack, Typography, ButtonGroup, Button, Chip, SvgIcon, Divider } from '@mui/material';
 import facePic from './resources/clear-bg-profile.png'
 import resume from './resources/ResumeMarch2022.pdf'
-import json from './contact.json'
+import vCard from './resources/GeorgeBrooks.vcf'
+import Flags from 'country-flag-icons/react/3x2'
 
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import EmailIcon from '@mui/icons-material/Email';
 import CallIcon from '@mui/icons-material/Call';
 import GitHubIcon from '@mui/icons-material/GitHub';
-import FilePresentOutlinedIcon from '@mui/icons-material/FilePresentOutlined';
-import PersonAddIcon from '@mui/icons-material/PersonAdd';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { JavaScriptIcon, NodeJSIcon, ReactIcon } from './resources/icons';
 
-const vcf = { ...json }
+
+
 function App() {
   return (
     <div className="App">
-    <Container>
-      <Avatar src={facePic} alt={(json.name.first + ' ' + json.name.last)} />
-      <Box>
-        <Typography>George Brooks</Typography>
-        <Typography>Web Developer</Typography>
-        <Typography>JavaScript. React. Node.js</Typography>
-      </Box>
-      <Paper elevation={1} marg>
+      <Container
+        maxWidth="xs" sx={{ background: "linear-gradient(180deg, #64ffda 15vh, #FFFFFF 15vh)" }}>
+        
+        <Stack justifyContent={"space-between"} sx={{ height: "90vh" }} mb={"5vh"} divider={<Divider/>}>
 
-      <ButtonGroup orientation='vertical' >
-        <Button>+1 (206) 476-3991</Button>
-        <Button>+351 912 620 081</Button>
-        <Button>george@georgebrooks.dev</Button>
-      </ButtonGroup>
+          <Stack alignItems={"center"} justifyContent={'flex-start'} pt={"5vh"} >
+            <Avatar
+              src={facePic}
+              alt={"George Brooks"}
+              sx={{height: "20vh", width: "20vh", backgroundColor: "#fff" }}
+            />
 
-</Paper>
-<Paper elevation={1}>
-        <Button>
-          <EmailIcon href="mailto:george@georgebrooks.dev"/>
-        </Button>
-        <Button>
-          <CallIcon href="tel:+12064763991"/>
+            <Typography variant="h3" component="h1">George Brooks</Typography>
+            <Typography variant="h4" component="h2">Web Developer</Typography>
+            
+            <Stack mt={4} className='skills' direction="row" justifyContent="space-evenly" spacing={2}>
+              <Chip variant='outlined' icon={<JavaScriptIcon />} label="JavaScript" />
+              <Chip variant='outlined' icon={<ReactIcon />} label="React" />
+              <Chip variant='outlined' icon={<NodeJSIcon />} label="Node.js" />
+            </Stack>
+          </Stack>
 
-        </Button>
-        <Button href={"https://linkedin.com/in/georgedsbrooks"}>
-          <LinkedInIcon />
-        </Button>
-        <Button href={"https://www.github.com/gdsbrooks"}>
-          <GitHubIcon />
-        </Button>
-        </Paper>
+          <Stack spacing={4} px={"5%"}>
+          <ButtonGroup orientation='vertical'>
+            <Button href={"tel:+12064763991"} startIcon={<SvgIcon><Flags.US /></SvgIcon>}>
+              +1 (206) 476-3991
+            </Button>
+            <Button href={"tel:+351912620081"} startIcon={<SvgIcon><Flags.PT /></SvgIcon>}>+351 912 620 081</Button>
+            </ButtonGroup>
+            <Button variant={"outlined"} href={"mailto:george@georgebrooks.dev"}>george@georgebrooks.dev</Button>
+            </Stack>
 
-      <Button href="#about_me" startIcon={<AccountCircleIcon />} variant="contained" color="primary" >
-        About Me
-      </Button>
+          <Stack direction="row" px={"5%"} alignItems="center" justifyContent="space-between">
+            <Button href={"mailto:george@georgebrooks.dev"} size="large"><EmailIcon/></Button>
+            <Button href={"tel:+12064763991"} size="large"> <CallIcon /> </Button>
+            <Button href={"https://linkedin.com/in/georgedsbrooks"} size="large"><LinkedInIcon /></Button>
+            <Button href={"https://www.github.com/gdsbrooks"} size="large"><GitHubIcon /></Button>
+          </Stack>
 
-      <Button href={resume} startIcon={<FilePresentOutlinedIcon />} variant="outlined" color="secondary">
-        My Resume
-      </Button>
+          <Stack direction="row" alignItems="center" justifyContent="space-between" px={"5%"}>
+            <Button
+              href={resume}
+              target="_blank"
+              variant="outlined"
+              color="secondary">
+              My Resume
+            </Button>
+            <Button
+              href={vCard}
+              type="text/x-vcard"
+              download={"georgebrooks.vcf"}
+              variant="contained"
+              color="primary"
+            >
+              Add Contact
+            </Button>
+          </Stack>
 
-      <Button href={"#addcontact"} startIcon={<PersonAddIcon />}>
-        Add Contact
-      </Button>
-      </Container>
-    </div>
+        </Stack>
+    </Container>
+    </div >
   );
 }
 
