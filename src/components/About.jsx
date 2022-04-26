b64ec00b7df72ed649cac394034ce710732e954d
George DS Brooks <george.ds.brooks@gmail.com>
2022-04-26 16:18:16 +0100

    added about bio. is responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,7 @@
-import { Box, Typography, Paper, Stack, LinearProgress, Divider } from '@mui/material'
+import { Box, Typography, Paper, Stack, LinearProgress, Divider, Grid } from '@mui/material'
 import facePic from '../resources/clear-bg-profile.png'
 import React from 'react'
+import { lineHeight } from '@mui/system';
 
 const skillset = [
   {
@@ -41,36 +42,59 @@ const skillset = [
 function About() {
   return (
     <React.Fragment>
-      <Stack direction='row' sx={{ flexWrap: 'wrap', gap: 2}}>
-        <Paper elevation={2} sx={{}}>
-          baljadlkjgag aldkfjalkdjg alkjgalkjglkjq aglksglkajglkj aslkjs;glkjqrglkj alkjq;lgkqglkj
-        </Paper>
-        <Paper elevation={2} sx={{px: 2, width: '100%', mx:'auto', maxWidth: '200px'}}>
-          <Stack spacing={1}>
-            <Typography variant='h5'>Skill Summary</Typography>
+      <Grid container spacing={2} justifyItems='stretch'>
 
-            {skillset.map((skill) => {
-              return (
-                <Box>
-                  <Typography variant='h6'>
-                    {skill.group}
-                  </Typography>
-                  {skill.skills.map((i) => {
-                    return (
-                      <Stack sx={{ my: 2 }}>
-                        <Typography variant='body'>
-                          {i.name}
-                        </Typography>
-                        <LinearProgress sx={{ }} variant="determinate" value={i.rating * 100} />
-                      </Stack>
-                    )
-                  })}
-                </Box>
-              )
-            })}
-          </Stack>
-        </Paper>
-      </Stack>
+        <Grid item xs={12} sm={9}>
+          <Paper elevation={2} sx={{p:2, lineHeight: "140%"}}>
+          <Typography variant='h5'>Professional Biography</Typography>
+
+          <Typography variant='body' >
+          <p>Full-stack Web Developer and <a href="http://www.ironhack.com" target='_blank' title="Ironhack Coding Bootcamp Website"> IronHack </a> bootcamp graduate (2021) building dynamic web applications and responsive front-ends. I'm competent in <strong className='js'>JavaScript ES6</strong> and have practice building web apps using <strong className='node'>Node.js</strong> and <strong className='react'>React</strong>, as well as handling RESTful API requests with <strong className='express'>Express</strong> and working with non-relational databases like <strong className='mongo'>MongoDB</strong> - the <strong>MERN</strong> stack.
+          </p>
+          <p>
+          My most recent position was as a contract frontend developer for <a href="#portfolio"> TheSignChef.com</a> delivering uniform <strong>accessibility standards</strong>, semantic HTML and <strong>Search Engine Optimization</strong> across the site, as well as successfully redesigning high-traffic pages to <strong>increase engagement</strong>. For this role I worked with the <strong>LAMP</strong> stack - and so I'm familiar using <strong>Linux, Apache, MySQL & PHP </strong> too.
+          </p>
+          <p>
+          I bring a decade of prior professional experience in healthcare, working as a <strong>pharmacist</strong> and all the leadership, management and interpersonal skills associated with a such a complex & challenging job. My experience working in numerous <strong> international markets</strong> and corresponding <strong>right to work in the US, UK and EU</strong> makes me an ideal candidate for a <strong>Work From Anywhere</strong>/ International remote position. I leverage strong communication skills in <strong>English</strong> (native) and <strong>Portuguese</strong> (spoken B1, reading & writing: C1) to engage multiple different stakeholders and harmonize the vision of multicultural teams.
+          </p>
+          <p>
+          I enjoy working in a cohesive <strong>Agile</strong> team as well as completing tasks independently. My goal is to find a role with significant opportunity to <strong>develop professionally</strong> and advance within a company that <strong>supports</strong> and <strong>empowers</strong> me to <strong>learn, grow and build.</strong>
+          </p>
+          <p>
+          Outside of work I enjoy exploring Portugal and its wealth of history and natural beauty, keeping fit with weightlifting and yoga and I have a soft spot for Daschunds.
+          </p>
+          </Typography>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Paper elevation={2} sx={{ p: 2}}>
+            <Stack spacing={1} direcion={{xs:'row', md:'column'}}>
+              <Typography variant='h5'>Skill Summary</Typography>
+
+              {skillset.map((skill) => {
+                return (
+                  <Box>
+                    <Typography variant='h6'>
+                      {skill.group}
+                    </Typography>
+                    {skill.skills.map((i) => {
+                      return (
+                        <Stack sx={{ my: 2 }}>
+                          <Typography variant='body'>
+                            {i.name}
+                          </Typography>
+                          <LinearProgress sx={{}} variant="determinate" value={i.rating * 100} />
+                        </Stack>
+                      )
+                    })}
+                  </Box>
+                )
+              })}
+            </Stack>
+          </Paper>
+        </Grid>
+
+      </Grid>
     </React.Fragment>
   )
 }
